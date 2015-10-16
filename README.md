e54d2bb6f0bfe1c9a213dfd82455a5bf18b8c3ce
Tom Smyth <tom@sassafras.coop>
2015-10-16 09:55:33 -0400

    Fixed to work with new URL

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ wider-google-tasks
 ==================
 
 Makes the Google Calendar tasks window wide enough to actually use!
+
+To install in chrome: download the .user.js file to your desktop, then drag to your extensions window and accept. Then go refresh the Google Calendar page. Enjoy!
--- wider-google-tasks.user.js
+++ wider-google-tasks.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name           Wider Google Tasks
 // @namespace      http://tomsmyth.ca
-// @match       https://www.google.com/calendar/*
+// @match          https://calendar.google.com/*
+// @match          https://www.google.com/calendar/*
 // ==/UserScript==
 
 setTimeout(function(){
   var TASKS_WIDTH = "250px";
-  var main = document.getElementById('gridcontainer'); 
-	var sidebar = document.getElementById('gadgetcell');
-	if (main && sidebar) {
-	  sidebar.style.width = TASKS_WIDTH;
-		sidebar.firstChild.firstChild.style.width = TASKS_WIDTH;
-		main.style.width = (main.offsetWidth - 88) + "px";
-	}
-	document.getElementById('rhstogglecell').style.display = "none";
+  var main = document.getElementById('gridcontainer');
+  var sidebar = document.getElementById('gadgetcell');
+  if (main && sidebar) {
+    sidebar.style.width = TASKS_WIDTH;
+    sidebar.firstChild.firstChild.style.width = TASKS_WIDTH;
+    main.style.width = (main.offsetWidth - 88) + "px";
+  }
+  document.getElementById('rhstogglecell').style.display = "none";
 }, 2000);
